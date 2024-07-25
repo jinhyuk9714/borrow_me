7aab8b06a510b2a20e6b9026e9af49273fb29593
jae0000 <jyyp3395@gmail.com>
2024-07-25 21:01:06 +0900

    소셜로그인 추가했음

--- src/main/java/com/ardkyer/rion/config/SecurityConfig.java
+++ src/main/java/com/ardkyer/rion/config/SecurityConfig.java
@@ -1,22 +1,22 @@
 package com.ardkyer.rion.config;
 
+import com.ardkyer.rion.service.PrincipalOauth2UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
+    private final PrincipalOauth2UserService principalOauth2UserService;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -27,16 +27,26 @@ public class SecurityConfig {
                 )
                 .formLogin(form -> form
                         .loginPage("/login")
-                        .loginProcessingUrl("/perform_login")
+                        //.loginProcessingUrl("/")
                         .defaultSuccessUrl("/home", true)
                         .failureUrl("/login?error=true")
-                        .permitAll()
+
                 )
+                .oauth2Login(login -> login
+                        .loginPage("/security-login/login")
+                        .defaultSuccessUrl("/security-login")
+                        .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint
+                                .userService(principalOauth2UserService))
+                )
+
+
+
                 .logout(logout -> logout
                         .logoutSuccessUrl("/login?logout")
-                        .permitAll()
+                        .invalidateHttpSession(true).deleteCookies("JSESSIONID")
                 );
 
         return http.build();
     }
+
 }
