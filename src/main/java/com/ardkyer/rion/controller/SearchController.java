47fce3b6eabb1c5cbae9f1d6cb457cd23d41a98f
ardkyer <k0207k@naver.com>
2024-08-05 06:29:35 +0900

    [Fix] search 에러 수정

--- src/main/java/com/ardkyer/rion/controller/SearchController.java
+++ src/main/java/com/ardkyer/rion/controller/SearchController.java
@@ -81,7 +81,7 @@ public class SearchController {
         model.addAttribute("videos", videos);
         model.addAttribute("exercises", exerciseService.getAllExercises());
         model.addAttribute("recentSearches", recentSearches);
-        return "searchResults";
+        return "searchResults.html";
     }
 
     @PostMapping("/search/recent/delete")
